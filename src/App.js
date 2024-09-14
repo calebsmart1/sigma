8290a5128037010dfa40e9d899904c159b11031e
calebsmart1 <caleb.osunagarcia1@gmail.com>
2024-09-14 17:34:24 -0400

    Updated files for sigma purposes

--- src/App.js
+++ src/App.js
@@ -4,11 +4,12 @@ import Greet from './components/greet.js';
 import Welcome from './components/welcome.js';
 import Hello from './components/hello.js';
 import Message from './components/message.js'
+import Counter from './components/Counter.js';
 
 function App() {
   return (
     <div className="App">
-      <Message />
+      <Counter />
     </div>
   );
 }
