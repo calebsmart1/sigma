793c19b8e5c6d529514ced3327ac49d6041448f7
calebsmart1 <caleb.osunagarcia1@gmail.com>
2024-09-14 15:52:43 -0400

    Sigma files

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import Greet from './components/greet.js';
+import Welcome from './components/welcome.js';
+import Hello from './components/hello.js';
+import Message from './components/message.js'
+
+function App() {
+  return (
+    <div className="App">
+      <Message />
+    </div>
+  );
+}
+
+export default App;
